c8331a0e68c1966a24e0774b7f12c93f214f7de8
Kyle Krick <krkrick@gmail.com>
2017-04-04 17:09:00 -0700

    Added card component ts class

---
+++ app/card/card.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'app-card',
+    templateUrl: 'card.component.html'
+})
+
+export class CardComponent {  }
